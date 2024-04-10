b566476d5c4a985eb808b4c3581544cb8b0e1bc0
Juan Pablo Sanchez Villegas <juanapablo@yconsultores.com>
2024-04-10 10:10:50 -0500

    key props

--- front/src/components/global/calendar.tsx
+++ front/src/components/global/calendar.tsx
@@ -13,7 +13,7 @@ export default function Calendar() {
       </article>
       <article className="grid grid-cols-7 mx-auto  w-[clamp(700px,80vw,1100px)]">
         {Array.from({ length: 30 }).map((_, i) => (
-          <div className="w-full aspect-square border border-black dark:border-white pl-1 pt-1">
+          <div className="w-full aspect-square border border-black dark:border-white pl-1 pt-1" key={i}>
             <p>{i}</p>
             {(i == 5 || i == 10 || i == 14 || i == 24 || i == 29) && (
               <ul className="list-inside">
