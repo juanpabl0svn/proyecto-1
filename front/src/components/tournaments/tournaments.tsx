98ee3a2b162c3e1ce7b4b3d9960fcc759ed79a9a
Juan Pablo Sanchez Villegas <juanapablo@yconsultores.com>
2024-05-27 02:22:53 -0500

    done couple of things

--- front/src/components/tournaments/tournaments.tsx
+++ front/src/components/tournaments/tournaments.tsx
@@ -8,19 +8,35 @@ export default function Tournaments() {
 
   const supabase = createClient();
 
-  const { isLoggedIn } = useUserContext();
+  const { isLoggedIn, user } = useUserContext();
+
+  const [isInTournament, setIsInTournament] = useState<any>({});
 
   useEffect(() => {
     (async () => {
       const { data, error } = await supabase.from("tournaments").select("*");
 
+      const { data: tournamentData, error: tournamentError } = await supabase
+        .from("tournaments_users")
+        .select("*")
+        .eq("id_user", user?.id_user);
+
+      if (tournamentData) {
+        const tournamentDict = tournamentData.reduce((acc: any, curr: any) => {
+          acc[curr.id_tournament] = true;
+          return acc;
+        }, {});
+
+        setIsInTournament(tournamentDict);
+      }
+
       if (data) {
         setTournaments(data);
       }
     })();
-  }, []);
+  }, [user?.id_user]);
 
-  const handleClick = (e: React.MouseEvent<HTMLElement>) => {
+  const handleClick = async (id_tournament: string) => {
     toast.remove();
     if (!isLoggedIn) {
       toast.error("Debes iniciar sesión para inscribirte en un torneo");
@@ -28,26 +44,33 @@ export default function Tournaments() {
       return;
     }
 
-    const { textContent } = e.target as HTMLElement;
-
-    if (textContent === "Inscribirme") {
-      toast.success("Te has inscrito en el torneo");
-      (e.target as HTMLElement).textContent = "Inscrito";
+    if (isInTournament[id_tournament]) {
+      await supabase
+        .from("tournaments_users")
+        .delete()
+        .eq("id_tournament", id_tournament);
+      const newObj = { ...isInTournament };
+      delete newObj[id_tournament];
+      setIsInTournament(newObj);
+    } else {
+      await supabase
+        .from("tournaments_users")
+        .insert([{ id_user: user?.id_user, id_tournament }]);
+      setIsInTournament({ ...isInTournament, [id_tournament]: true });
     }
   };
 
   return (
     <main className="pt-28 flex justify-center gap-4 flex-wrap bg-gray-300 min-h-dvh h-full pb-10">
       {tournaments.map((tournament: any) => {
-        // const isInscribed = user.tournaments
-
         return (
           <section
-            key={tournament.id}
+            key={tournament.id_tournament}
             className="w-[600px] aspect-square p-4 border border-black rounded-sm bg-white"
           >
             <img src="/tournament.png" alt="" />
             <h2 className="text-2xl font-bold">{tournament.title}</h2>
+            <p>{tournament.id_tournament}</p>
 
             <article className="flex w-full justify-between px-4">
               <div>
@@ -61,12 +84,19 @@ export default function Tournaments() {
                   {tournament.prize}
                 </p>
               </div>
-              <button
-                onClick={handleClick}
-                className="bg-blue-500 rounded-md p-2 text-white hover:bg-blue-800 transition-all duration-200 ease-in-out"
-              >
-                Inscribirme
-              </button>
+              <div className="flex flex-col">
+                <button
+                  onClick={() => handleClick(tournament.id_tournament)}
+                  className="bg-blue-500 rounded-md p-2 text-white hover:bg-blue-800 transition-all duration-200 ease-in-out"
+                >
+                  {isInTournament[tournament.id_tournament]
+                    ? "Inscrito"
+                    : "Inscribirme"}
+                </button>
+                {isInTournament[tournament.id_tournament] && (
+                  <span className="text-sm text-gray-400">Oprima para desinscribir</span>
+                )}
+              </div>
             </article>
           </section>
         );
